634955cdd096a4fbb2b95463adb802ab06c0edc4
idolakampus <delasilvira26@gmail.com>
2026-07-19 00:19:07 +0700

    Add files via upload

---
+++ script[1].js
@@ -0,0 +1,4 @@
+function order(paket){
+const msg=`Halo Admin Nanas Premium 👋%0A%0ASaya ingin order.%0A%0AProduk: ${paket}%0ANama:%0AEmail:%0ADurasi:%0A%0ATerima kasih.`;
+window.open("https://wa.me/6287733623244?text="+msg,"_blank");
+}
